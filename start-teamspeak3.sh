8225116a54cd5f43c09f2bc68047ade3e9e4f2ab
Andreas Heil <ah@linux-hq.de>
2017-05-31 15:33:14 +0200

    do not download everytime we start (bug introduces by "rm -f"-patch)

--- start-teamspeak3.sh
+++ start-teamspeak3.sh
@@ -10,11 +10,22 @@ cd /data
 
 TARFILE=teamspeak3-server_linux_amd64-${TS_VERSION}.tar.bz2
 
-if [ ! -e ${TARFILE} ]; then
+download=0
+if [ ! -e version ]; then
+  download=1
+else
+  read version <version
+  if [ "$version" != "$TS_VERSION" ]; then
+    download=1
+  fi
+fi
+
+if [ "$download" -eq 1 ]; then
   echo "Downloading ${TARFILE} ..."
   wget -q http://dl.4players.de/ts/releases/${TS_VERSION}/${TARFILE} \
   && tar -j -x -f ${TARFILE} --strip-components=1 \
-  && rm -f ${TARFILE}
+  && rm -f ${TARFILE} \
+  && echo $TS_VERSION >version
 fi
 
 export LD_LIBRARY_PATH=/data
